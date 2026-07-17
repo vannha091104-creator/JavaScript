37bc1a4c10cd4f11d17b8bdcd7cc4fe1f577c6b7
Nha Nguyen <vannha091104@gmail.com>
2026-07-17 11:34:37 +0700

    update button selector after renaming button id

--- exercise1-the-logo-hijack.html.js
+++ exercise1-the-logo-hijack.html.js
@@ -1,5 +1,5 @@
 var img = document.getElementById('hplogo');
 img.width = 400;
 img.src = 'http://www.logostage.com/logos/yahoo.GIF';
-var button = document.getElementById('gbqfba');
-button.innerHTML = 'Yahoooo!';
+var button = document.getElementById('search');
+button.textContent = 'Yahoooo!';
